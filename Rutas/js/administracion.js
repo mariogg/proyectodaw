687ee91758e3cb2ad3dc46ecceac02d73879485f
jscordero <juansancor83@gmail.com>
2017-03-20 18:31:08 +0100

    Modificaciones

--- Rutas/js/administracion.js
+++ Rutas/js/administracion.js
@@ -1,11 +1,28 @@
+var fileExtension = "";
+var fileName="";
 
-
-$(document).ready(function(){
+$(document).ready(function(){	
 	$("#registro").click(recogerDatos)
+	 
+    //función que observa los cambios del campo file y obtiene información
+    $(':file').change(function()
+    {
+        //obtenemos un array con los datos del archivo
+        var file = $("#imagen")[0].files[0];
+        //obtenemos el nombre del archivo
+        fileName = file.name;
+        //obtenemos la extensión del archivo
+        fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+        //obtenemos el tamaño del archivo
+        var fileSize = file.size;
+        //obtenemos el tipo de archivo image/png ejemplo
+        var fileType = file.type;
+        //mensaje con la información del archivo
+        showMessage("<span class='info'>Archivo para subir: "+fileName+", peso total: "+fileSize+" bytes.</span>");
+    });
 })
 
-function recogerDatos(){
-
+function datos(direc){	 
 	var datos=$("#nuevo_articulo input")	
 	var nombre, kilometros, minutos, inicio, destino, consejos, dificultad, num_reservas, direc, mapa
 	nombre=datos[0].value
@@ -14,8 +31,8 @@ function recogerDatos(){
 	inicio=datos[3].value
 	destino=datos[4].value
 	num_reservas=parseInt(datos[5].value)
-	direc=datos[6].value
 	mapa=datos[7].value
+	archivo=direc,
 	consejos=$('textarea').val()
 	dificultad=$('select').val()
 	var ruta={
@@ -27,19 +44,40 @@ function recogerDatos(){
 		maximo:num_reservas,
 		mapa:mapa,
 		dificultad:dificultad,
-		pdf:direc,
+		archivo:archivo,
 		consejos:consejos
 	}
 	
 	$.ajax({
 		
-		method:"POST",		
-		//dataType:"json",
+		type:"POST",		
+		//dataType:"json",        
 		url:"Rutas/php/panel_admin_registro.php",		
-		data:ruta,
+		data:ruta,		
 		success:function(data){
 			$("#mensaje").html(data)
 		}
 		
 	})
+	
 }
+
+function recogerDatos(){
+	 var formData = new FormData($(".formulario")[0]);
+	 $.ajax({
+		url: 'Rutas/php/direccion_imagen.php',  
+		type: 'POST',
+		// Form data
+		//datos del formulario
+		data: formData,
+		//necesario para subir archivos via ajax
+		cache: false,
+		contentType: false,
+		processData: false,
+		
+		//una vez finalizado correctamente
+		success: function(data){
+			document.write(data)
+		}
+}
+
---
+++ Rutas/php/direccion_imagen.php
@@ -0,0 +1,22 @@
+<?php
+//comprobamos que sea una petición ajax
+if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') 
+{
+ 
+    //obtenemos el archivo a subir
+    $file = $_FILES['archivo']['name'];
+ 
+    //comprobamos si existe un directorio para subir el archivo
+    //si no es así, lo creamos
+    if(!is_dir("pdf/")) 
+        mkdir("pdf/");
+     
+    //comprobamos si el archivo ha subido
+    if ($file && move_uploaded_file($_FILES['archivo']['tmp_name'],"files/".$file))
+    {
+       
+       echo $file;//devolvemos el nombre del archivo para pintar la imagen
+    }
+}else{
+    throw new Exception("Error Processing Request", 1);   
+}
