0e97066776eeadb7af4210eee3a844210f7f55b8
jscordero <juansancor83@gmail.com>
2017-04-17 18:04:08 +0200

    terminado panel hospedaje

--- Usuarios/js/adminhospedaje.js
+++ Usuarios/js/adminhospedaje.js
@@ -12,12 +12,14 @@ function mostrar(){
                 var enlace="<table>"				
 				for( var x=0;x<data.length;x++){
 					enlace+="<tr>"
-					enlace+="<td><button class='borrar' id='"+data[x].id+"'>Borrar</button></td><td>"+data[x].id+"</td><td>"+data[x].nombre+"</td><td>"+data[x].localidad+"</td><td>"+data[x].descripcion+"</td><td>"+data[x].web+"</td>"
+					enlace+="<td><button class='borrar' id='"+data[x].id+"'>Borrar</button><button class='modificar' id='"+data[x].id+"'>Modificar</button></td><td>"+data[x].id+"</td><td>"+data[x].nombre+"</td><td>"+data[x].localidad+"</td><td>"+data[x].descripcion+"</td><td>"+data[x].web+"</td>"
 					enlace+="</tr>"
 				}
 				enlace+="</table>"
 				$('#mostrar').html(enlace)
 				$('.borrar').click(borrar)
+				$('.modificar').click(modificar)
+				$('#actualizar').click(actualizar)
             }            
         })		
 }
@@ -28,16 +30,14 @@ function guardarHospedaje(){
 	var localidad=$('#localidad').val()
 	var direccion=datos[2].value
 	var telefono=datos[3].value
-	var telefono2=datos[4].value
-	var email=datos[5].value
-	var web=datos[6].value
+	var email=datos[4].value
+	var web=datos[5].value
 	
 	var envio={
 		nombre:nombre,
 		localidad:localidad,
 		direccion:direccion,
 		telefono:telefono,
-		telefono2:telefono2,
 		email:email,
 		web:web
 	}
@@ -60,6 +60,7 @@ $(document).ready(function(){
 })
 
 function borrar(){
+	$('#mensaje').html("")
 	var id=$(this).attr("id")
 	envia={
 		id:id
@@ -68,9 +69,67 @@ function borrar(){
 		url:'Usuarios/php/borrarHospedaje.php',
 		type:'post',
 		data:envia,                     
-		success:function(respuesta) {			
+		success:function(respuesta) {
+								
 			$('#mensaje').html(respuesta)			
 			mostrar()
+		}            
+	})
+}
+
+function modificar(){
+	$('#mensaje').html("")
+	var id=$(this).attr("id")
+	envia={
+		id:id
+	}	
+	$.ajax({            
+		url:'Usuarios/php/modificarHospedaje.php',
+		type:'post',
+		DataType:'Json',
+		data:envia,                     
+		success:function(datos) {			
+			console.log(datos[0].id)
+			$('#localidad').val(datos[0].localidad)	
+			$('#annadir input[name=id]').val(datos[0].id)
+			$('#annadir input[name=nombre]').val(datos[0].nombre)
+			$('#annadir input[name=direccion]').val(datos[0].direccion)
+			$('#annadir input[name=telefono]').val(datos[0].telefono)
+			$('#annadir input[name=email]').val(datos[0].email)
+			$('#annadir input[name=web]').val(datos[0].web)
+			mostrar()
+					
+		}            
+	})
+}
+
+function actualizar(){
+	var datos=$('#annadir input')
+	var id=datos[0].value
+	var nombre=datos[1].value
+	var localidad=$('#localidad').val()
+	var direccion=datos[2].value
+	var telefono=datos[3].value
+	var email=datos[4].value
+	var web=datos[5].value
+	
+	var envio={
+		id:id,
+		nombre:nombre,
+		localidad:localidad,
+		direccion:direccion,
+		telefono:telefono,
+		email:email,
+		web:web
+	}
+	$.ajax({            
+		url:'Usuarios/php/editarHospedaje.php',
+		type:'post',
+		data:envio,                     
+		success:function(respuesta) {	
+			$('#mensaje').html("")	
+			mostrar()
+			$('#mensaje').html(respuesta)	
 		}            
 	})
 }
