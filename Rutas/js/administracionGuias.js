4743d90ca0b3966e771c004905e8e7039ef01186
jscordero <juansancor83@gmail.com>
2017-05-01 19:33:48 +0200

    correcion imagen

--- Rutas/js/administracionGuias.js
+++ Rutas/js/administracionGuias.js
@@ -2,39 +2,48 @@
 $(document).ready(function() {
     cargarDatos();
     var fileExtension="";
-    $("insertGuia :file").change(function() {
-        var file = $("#fotoGuia")[0].files[0];
-        var fileName = file.name;
-        fileExtension = fileName.substring(fileName.lastIndexOf(".")+1);
+    $('#nav :file').change(function(){
+        //obtenemos un array con los datos del archivo
+        var file = $("#imagen")[0].files[0]
+        //obtenemos el nombre del archivo
+        var fileName = file.name
+        //obtenemos la extensión del archivo
+        fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1)
+        //obtenemos el tamaño del archivo
+        //var fileSize = file.size
+        //obtenemos el tipo de archivo image/png ejemplo
+        //var fileType = file.type
+        //mensaje con la información del archivo        
     });
+	
     $('#insertar').on('click',function() {
-        var nick = $(this).parent().parent().children().children("#nombreGuia").val();
+        /*var nick = $(this).parent().parent().children().children("#nombreGuia").val();
         console.log(nick);
         var experiencia = $(this).parent().parent().children().children("#experiencia").val();
         console.log(experiencia);
         var imagen = $(this).parent().parent().children().children("#fotoGuia").val();
         console.log(imagen);
 
-
-
-
         var enviarAjax = {"nick":nick,"experiencia":experiencia,"imagen":imagen};
-        console.log(enviarAjax);
+        console.log(enviarAjax);*/
+		
+		
 
         var formData = new FormData($(".formulario")[0]);
 
-        $.ajax({
-
-            url:   'Rutas/php/direccion_imagen.php',
-            type:  'post',
-            data: 'formData',
+         $.ajax({
+            url: 'rutas/php/direccion_imagen.php',  
+            type: 'POST',
+            // Form data
+            //datos del formulario
+            data: formData,
+            //necesario para subir archivos via ajax
             cache: false,
-            contentType: false,
+            contentType: false,			
             processData: false,
-            beforeSend: function () {
-                $("#resultado").html("Procesando, espere por favor...");
-            },
-            success:  function (response) {
+            
+            //una vez finalizado correctamente
+            success: function(response){
 
 
 
